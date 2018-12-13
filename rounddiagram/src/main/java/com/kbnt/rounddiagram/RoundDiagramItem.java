183abb1221c1916ce6352c14891e890f0e33e5d4
kam.tayupov <kam.kbnt@gmail.com>
2018-12-13 09:10:16 +0300

    split normal text and count text

--- rounddiagram/src/main/java/com/kbnt/rounddiagram/RoundDiagramItem.java
+++ rounddiagram/src/main/java/com/kbnt/rounddiagram/RoundDiagramItem.java
@@ -7,6 +7,7 @@ import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.constraint.ConstraintLayout;
 import android.util.AttributeSet;
+import android.util.TypedValue;
 import android.view.LayoutInflater;
 import android.widget.TextView;
 
@@ -14,6 +15,7 @@ public class RoundDiagramItem extends ConstraintLayout {
 
     private RoundDialView mDial;
     private TextView mCount;
+    private TextView mText;
 
     public RoundDiagramItem(@NonNull Context context) {
         super(context);
@@ -34,18 +36,31 @@ public class RoundDiagramItem extends ConstraintLayout {
         final TypedArray a = getContext().obtainStyledAttributes(
                 attrs, R.styleable.RoundDiagramItem, defStyle, 0);
         String label = a.getString(R.styleable.RoundDiagramItem_label);
+        int textSize = a.getDimensionPixelSize(R.styleable.RoundDiagramItem_textSize, 0);
+        int countTextSize = a.getDimensionPixelSize(R.styleable.RoundDiagramItem_countSize, 0);
         a.recycle();
 
         LayoutInflater inflater = (LayoutInflater) getContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-        inflater.inflate(R.layout.layout_item, this);
+        inflater.inflate(R.layout.layout_diagram_item, this);
         ((TextView) findViewById(R.id.label)).setText(label);
 
         mDial = findViewById(R.id.dial);
         mCount = findViewById(R.id.count);
+        mText = findViewById(R.id.text);
+        if (textSize > 0) {
+            mText.setTextSize(TypedValue.COMPLEX_UNIT_SP, textSize);
+        }
+        if (countTextSize > 0) {
+            mCount.setTextSize(TypedValue.COMPLEX_UNIT_SP, countTextSize);
+        }
     }
 
     public void setCount(@IntRange(from = 0) int count) {
         mCount.setText(String.valueOf(count));
+    }
+
+    public void setText(String text) {
+        mText.setText(text);
     }
 
     public void setDial(@IntRange(from = 0, to = 100) int acc, @IntRange(from = 0, to = 100) int vel) {
